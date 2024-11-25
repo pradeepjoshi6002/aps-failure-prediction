3608217ae678df8a9bec298c8528f00aefb3ec32
Pradeep-Joshi-ob <pradeep.joshi@ext.officebanao.in>
2024-11-25 23:41:44 +0530

    package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sensor',
+    version="0.0.1",
+    author='Pradeep',
+    author_email='pradeepjoshi6002@gmail.com',
+    packages=find_packages(),
+    install_requires=["pymongo"]
+)
